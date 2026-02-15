e18af0f42b507e465caada0d04cb972014fcf825
abhiisdev7 <madiabhilash1563@gmail.com>
2026-02-15 08:21:10 +0530

    fix: build error due to mongo uri may undefined

--- lib/db/mongoose.ts
+++ lib/db/mongoose.ts
@@ -32,7 +32,7 @@ export async function connectDB(): Promise<typeof mongoose> {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI);
+    cached.promise = mongoose.connect(MONGODB_URI!);
   }
 
   cached.conn = await cached.promise;
